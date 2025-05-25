8ffa9018e7244244e60f0b6a54f589c34f621bbc
Papazy <fajryjobs@gmail.com>
2025-05-25 16:02:21 +0700

    fix routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,59 @@
-import { Leaf } from 'lucide-react'
-import Head from 'next/head'
-import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-import NavMenu from '#components/common/NavMenu'
-import { AppConfig } from '#lib/AppConfig'
+const Home = () => {
+  const router = useRouter()
 
-const Home = () => (
-  <div className="container mx-auto max-w-2xl  p-3 max-md:max-w-none">
-    <Head>
-      <title>Jumpstart your new leaflet mapping Project with next.js and typescript 🤩</title>
-      <meta
-        property="og:title"
-        content="Jumpstart your new leaflet mapping Project with next.js and typescript 🤩"
-        key="title"
-      />
-      <meta
-        name="description"
-        content="next-leaflet-starter-typescript is an extensible next.js starter template for the leaflet-maps-react plugin. Written in typescript,
-      visually enhanced by tailwind and lucide-react icons."
-      />
-    </Head>
-    <header className="items-top mt-10 gap-4 md:flex">
-      <span className="text-primary">
-        <Leaf size={AppConfig.ui.bigIconSize} className="mt-2" />
-      </span>
-      <div>
-        <h2 className="text-4xl font-bold ">Next.js starter for leaflet-react</h2>
-        <h3 className="mb-16 text-3xl">written in Typescript</h3>
-      </div>
-    </header>
+  useEffect(() => {
+    router.push('/map')
+  }, [router])
 
-    <section className="grid grid-cols-1 md:grid-cols-2">
-      <div>
-        <h3 className="my-5 text-xl">Demo Content</h3>
-        <NavMenu />
-      </div>
-    </section>
-  </div>
-)
+  return null
+}
 
 export default Home
+
+
+// import { Leaf } from 'lucide-react'
+// import Head from 'next/head'
+// import Link from 'next/link'
+
+// import NavMenu from '#components/common/NavMenu'
+// import { AppConfig } from '#lib/AppConfig'
+
+// const Home = () => (
+//   <div className="container mx-auto max-w-2xl  p-3 max-md:max-w-none">
+//     <Head>
+//       <title>Jumpstart your new leaflet mapping Project with next.js and typescript 🤩</title>
+//       <meta
+//         property="og:title"
+//         content="Jumpstart your new leaflet mapping Project with next.js and typescript 🤩"
+//         key="title"
+//       />
+//       <meta
+//         name="description"
+//         content="next-leaflet-starter-typescript is an extensible next.js starter template for the leaflet-maps-react plugin. Written in typescript,
+//       visually enhanced by tailwind and lucide-react icons."
+//       />
+//     </Head>
+//     <header className="items-top mt-10 gap-4 md:flex">
+//       <span className="text-primary">
+//         <Leaf size={AppConfig.ui.bigIconSize} className="mt-2" />
+//       </span>
+//       <div>
+//         <h2 className="text-4xl font-bold ">Next.js starter for leaflet-react</h2>
+//         <h3 className="mb-16 text-3xl">written in Typescript</h3>
+//       </div>
+//     </header>
+
+//     <section className="grid grid-cols-1 md:grid-cols-2">
+//       <div>
+//         <h3 className="my-5 text-xl">Demo Content</h3>
+//         <NavMenu />
+//       </div>
+//     </section>
+//   </div>
+// )
+
+// export default Home
+
